f7cc012d00a26a8c6c4805458747d3d0ce9e9d43
EltonJitiako <eltonjitiako@gmail.com>
2022-12-28 16:01:45 +0100

    spring heroku

---
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.Key;
+
+//@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.getenv().forEach((key, value) -> System.out.println(key + " = " + value));
+	}
+
+}
